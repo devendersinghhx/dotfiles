58c1d113c0ae2f21fa91d8d5d139099ddf057610
Devender <devendercoding@gmail.com>
2024-09-15 23:25:23 +0530

    lualine customized

--- .config/nvim/lua/deven/plugins/lualine.lua
+++ .config/nvim/lua/deven/plugins/lualine.lua
@@ -6,8 +6,10 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+				--component_separators = { left = "", right = "" },
+				--section_separators = { left = "", right = "" },
+				component_separators = { "|" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
