89c88b94dfb6aaa7835d1d5a04b3622677fc1c2d
Devender <devendercoding@gamil.com>
2024-12-25 19:11:36 +0530

    yazi new config

---
+++ .config/yazi/init.lua
@@ -0,0 +1,6 @@
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.PLAIN,
+})
+require("no-status"):setup()
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
