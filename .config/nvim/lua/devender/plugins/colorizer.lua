8651fd0a2c51be1cbc33bfeb6b43f0477ef97260
Devender <devendercoding@gamil.com>
2025-01-08 21:15:16 +0530

    No longer using colorizer

--- .config/nvim/lua/devender/plugins/colorizer.lua
+++ .config/nvim/lua/devender/plugins/colorizer.lua
@@ -1,6 +1,7 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	event = { "BufReadPre", "BufNewFile" },
+	enabled = false,
 	config = function()
 		vim.o.termguicolors = true
 		local colorizer = require("colorizer")
