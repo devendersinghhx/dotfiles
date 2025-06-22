91a379a6d93594e4272610a657ca51381047eeca
Devender <devendercoding@gmail.com>
2025-06-22 11:27:19 +0530

    changes

--- .vscode/extensions/vadimcn.vscode-lldb-1.11.5/adapter/scripts/codelldb/commands/nofail.py
+++
@@ -1,8 +0,0 @@
-class NoFailCommand:
-    '''Runs another command ignoring failures.'''
-
-    def __init__(self, debugger, internal_dict):
-        pass
-
-    def __call__(self, debugger, command, exe_ctx, result):
-        debugger.HandleCommand(command)
--- .vscode/extensions/zarifprogrammer.tailwind-snippets-1.0.2/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-## Release Notes
-
-Here you will find the release notes of Tailwind Snippets.
-
-### 1.0.0
-
-Initial release of Tailwind Snippets
-
----
